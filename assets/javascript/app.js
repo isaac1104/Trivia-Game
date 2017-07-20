276876edd82b2eec17efb31d55ec1bc80c0a4250
I Chan Kwon <ichankwon@Is-MacBook-Pro.local>
2017-07-19 22:14:35 -0700

    Third Commit: Cleaned Up Code to Stay DRY

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,290 +1,128 @@
 //Variables
-var time = 2;
+var time = 16;
 var intervalId;
+var correctGuess = 0;
+var wrongGuess = 0;
+var questionNumber = 0;
 var timeRunning = false;
-
-//Functions//
-function showDisplay() {
-  $("#time-left").fadeIn();
-  $("#question").fadeIn();
-  $("#choices").fadeIn();
+//Trivia question objects//
+var trivia = {
+  questions: [{
+      question: "Which player has won the most NBA championships?",
+      answer: "Bill Russell",
+      explanation: "Bill Russell has won the most NBA championships with total of 16 championship wins playing for the Boston Celtics",
+      multipleChoice: ["Bill Russell", "Michael Jordan", "Tim Duncan", "Kobe Bryant"]
+    },
+    {
+      question: "Which NBA team has won the most NBA championships?",
+      answer: "Boston Celtics",
+      explanation: "Boston Celtics have won the NBA championships 17 times. Los Angeles Lakers are right behind the Celtics with 16 wins.",
+      multipleChoice: ["Los Angeles Lakers", "Indiana Pacers", "Boston Celtics", "Golden State Warriors"]
+    },
+    {
+      question: "Who is the NBA's all-time scoring leader?",
+      answer: "Kareem Abdul-Jabbar",
+      explanation: "The NBA's top scorer is Kareem Abdul-Jabbar, with 38,387 points. Karl Malone, Kobe Bryant and Michael Jordan are 2nd, 3rd, and 4th all-time scorers in the league.",
+      multipleChoice: ["Lebron James", "Michael Jordan", "Kobe Bryant", "Kareem Abdul-Jabbar"]
+    },
+    {
+      question: "What is Kobe Bryant's career high score?",
+      answer: "81 Points",
+      explanation: "In 2006, Bryant scored a career-high 81 points against the Toronto Raptors. It was the second-highest number of points scored in a game in NBA history, behind only Chamberlain's 100-point performance in 1962.",
+      multipleChoice: ["69 Points", "73 Points", "85 Points", "81 Points"]
+    },
+    {
+      question: "Which player was the NBA 2017 finals MVP?",
+      answer: "Kevin Durant",
+      explanation: "Kevin Durant was named the 2017 Bill Russell NBA Finals MVP following the Warriors’ Game 5 elimination victory over the Cleveland Cavaliers on Monday. He capped off the series with a 39-point performance to lead the Warriors to a 129-120 win.",
+      multipleChoice: ["Stephen Curry", "Kevin Durant", "Klay Thompson", "Andre Igoudala"]
+    },
+    {
+      question: "In the 2010 NBA Finals, the Lakers beat the Celtics in which game?",
+      explanation: "This game was the first Game 7 in an NBA Finals since 2005 five years earlier. Los Angeles Lakers beat the Celtics 83 to 79 and won their 16th NBA championship title.",
+      answer: "Game 7",
+      multipleChoice: ["Game 5", "Game 4", "Game 7", "Game 6"]
+    },
+  ]
+};
+//Function that displays the result at the end//
+function displayResults() {
+  $("#displayResult").show();
+  $("#display").hide();
+  $("#wins").html("Total number of correct guesses: " + correctGuess);
+  $("#losses").html("Total number of incorrect guesses: " + wrongGuess);
+  $("#replay").fadeIn();
 }
-
-function hideButtons() {
-  $("#next").hide();
-  $("#next2").hide();
-  $("#next3").hide();
-  $("#next4").hide();
-  $("#next5").hide();
-  $("#result").hide();
+//Function that reloads the game back to the first question//
+function startGame() {
+  questionNumber = 0;
+  askQuestion(questionNumber);
+  $("#display").fadeIn();
+  $("#start").fadeOut();
+  $("#replay").fadeOut();
 }
-
-function displayExplanation() {
-  $("#time-left").hide();
-  $("#question").hide();
-  $("#choices").hide();
-  $("#rightOrWrong").show();
-  $("#explain").show();
+//Function that generates questions from the trivia object. questionNumber parameter represents the number of questions in the array.//
+function askQuestion(questionNumber) {
+  //Sets the timer to 15 seconds on every new questions//
+  time = 15;
+  timer();
+  if (questionNumber < trivia.questions.length) {
+    $("#question").html("<h3>" + trivia.questions[questionNumber].question + "</h3>");
+    $(".choice-1").html(trivia.questions[questionNumber].multipleChoice[0]);
+    $(".choice-2").html(trivia.questions[questionNumber].multipleChoice[1]);
+    $(".choice-3").html(trivia.questions[questionNumber].multipleChoice[2]);
+    $(".choice-4").html(trivia.questions[questionNumber].multipleChoice[3]);
+  }
 }
-
+//Function that checks to see if the answer clicked is the right answer//
+function checkAnswer(guess) {
+  if (guess === trivia.questions[questionNumber].answer) {
+    return true;
+  } else {
+    return false;
+  }
+}
+//Timer that counts down to 0//
 function timer() {
-  time--;
-  $("#time-left").html("Time Left: " + time);
-  intervalId = setTimeout(timer, 1000);
-}
-
-//Functions that generate questions & answers//
-//First question & answer explanation//
-function firstQuestion() {
-  $("#question").html("What is the first question?");
-  $(".choice-1").html("1. Correct answer");
-  $(".choice-2").html("2. Wrong answer");
-  $(".choice-3").html("3. Wrong answer");
-  $(".choice-4").html("4. Wrong answer");
-  $(".answer-choices").on("click", function() {
-    if (($(this).attr("value") === "1")) {
-      $("#rightOrWrong").html("You are correct!");
-      explainAnswer1();
-    } else if (time === 0) {
+    time--;
+    $("#time-left").html("<h4>Time Left: " + time + " seconds</h4>");
+    if (time === 0) {
       clearInterval(intervalId);
-      $("#rightOrWrong").html("Wrong answer!");
-      explainAnswer1();
+      questionNumber++;
+      wrongGuess++;
+      askQuestion(questionNumber);
+    } else if (questionNumber === trivia.questions.length) {
+      clearInterval(intervalId);
+      displayResults();
     } else {
-      $("#rightOrWrong").html("Wrong answer!");
-      explainAnswer1();
-    }
-  });
-}
-
-function explainAnswer1() {
-  clearInterval(intervalId);
-  displayExplanation();
-  $("#explain").html("The answer is one because blah blah blah");
-
-  function nextButton() {
-    $("#next").fadeIn();
+      intervalId = setTimeout(timer, 1000);
   }
-  setTimeout(nextButton, 3000);
 }
 
-//Second question & answer explanation//
-function secondQuestion() {
-  $("#question").html("What is the second question?");
-  $(".choice-1").html("1. Wrong answer");
-  $(".choice-2").html("2. Wrong answer");
-  $(".choice-3").html("3. Correct answer");
-  $(".choice-4").html("4. Wrong answer");
-  $(".answer-choices").on("click", function() {
-    if (($(this).attr("value") === "3")) {
-      $("#rightOrWrong").html("You are correct!");
-      explainAnswer2();
-    } else {
-      $("#rightOrWrong").html("Wrong answer!");
-      explainAnswer2();
-    }
-  });
-}
-
-function explainAnswer2() {
-  clearInterval(intervalId);
-  displayExplanation();
-  $("#explain").html("The answer is three because blah blah blah");
-
-  function nextButton2() {
-    $("#next2").fadeIn();
-    $("#next").hide();
-  }
-  setTimeout(nextButton2, 3000);
-}
-
-//Third question & answer explanation//
-function thirdQuestion() {
-  $("#question").html("What is the third question?");
-  $(".choice-1").html("1. Wrong answer");
-  $(".choice-2").html("2. Wrong answer");
-  $(".choice-3").html("3. Wrong answer");
-  $(".choice-4").html("4. Correct answer");
-  $(".answer-choices").on("click", function() {
-    if (($(this).attr("value") === "4")) {
-      $("#rightOrWrong").html("You are correct!");
-      explainAnswer3();
-    } else {
-      $("#rightOrWrong").html("Wrong answer!");
-      explainAnswer3();
-    }
-  });
-}
-
-function explainAnswer3() {
-  clearInterval(intervalId);
-  displayExplanation();
-  $("#explain").html("The answer is four because blah blah blah");
-
-  function nextButton3() {
-    $("#next3").fadeIn();
-    $("#next2").hide();
-  }
-  setTimeout(nextButton3, 3000);
-}
-
-//Fourth question & answer explanation//
-function fourthQuestion() {
-  $("#question").html("What is the fourth question?");
-  $(".choice-1").html("1. Wrong answer");
-  $(".choice-2").html("2. Correct answer");
-  $(".choice-3").html("3. Wrong answer");
-  $(".choice-4").html("4. Wrong answer");
-  $(".answer-choices").on("click", function() {
-    if (($(this).attr("value") === "2")) {
-      $("#rightOrWrong").html("You are correct!");
-      explainAnswer4();
-    } else {
-      $("#rightOrWrong").html("Wrong answer!");
-      explainAnswer4();
-    }
-  });
-}
-
-function explainAnswer4() {
-  clearInterval(intervalId);
-  displayExplanation();
-  $("#explain").html("The answer is two because blah blah blah");
-
-  function nextButton4() {
-    $("#next4").show();
-    $("#next3").hide();
-  }
-  setTimeout(nextButton4, 3000);
-}
-
-//Fifth question & answer explanation//
-function fifthQuestion() {
-  $("#question").html("What is the fifth question?");
-  $(".choice-1").html("1. Wrong answer");
-  $(".choice-2").html("2. Wrong");
-  $(".choice-3").html("3. Correct answer");
-  $(".choice-4").html("4. Wrong answer");
-  $(".answer-choices").on("click", function() {
-    if (($(this).attr("value") === "3")) {
-      $("#rightOrWrong").html("You are correct!");
-      explainAnswer5();
-    } else {
-      $("#rightOrWrong").html("Wrong answer!");
-      explainAnswer5();
-    }
-  });
-}
-
-function explainAnswer5() {
-  clearInterval(intervalId);
-  displayExplanation();
-  $("#explain").html("The answer is three because blah blah blah");
-
-  function nextButton5() {
-    $("#next5").show();
-    $("#next4").hide();
-  }
-  setTimeout(nextButton5, 3000);
-}
-
-//Sixth & last question & answer explanation//
-function sixthQuestion() {
-  $("#question").html("What is the sixth question?");
-  $(".choice-1").html("1. Wrong answer");
-  $(".choice-2").html("2. Wrong answer");
-  $(".choice-3").html("3. Wrong answer");
-  $(".choice-4").html("4. Correct answer");
-  $(".answer-choices").on("click", function() {
-    if (($(this).attr("value") === "4")) {
-      $("#rightOrWrong").html("You are correct!");
-      explainAnswer6();
-    } else {
-      $("#rightOrWrong").html("Wrong answer!");
-      explainAnswer6();
-    }
-  });
-}
-
-function explainAnswer6() {
-  clearInterval(intervalId);
-  displayExplanation();
-  $("#explain").html("The answer is four because blah blah blah");
-
-  function nextButton6() {
-    $("#result").show();
-    $("#next5").hide();
-  }
-  setTimeout(nextButton6, 3000);
-}
-
-//Start Game//
 $(document).ready(function() {
-  hideButtons();
-  //On button click, display content, count down begins & show the first question//
+  $("#replay").hide();
+  $("#next").hide();
   $("#start").on("click", function() {
-    if (!timeRunning) {
-      timeRunning = true;
-      $("#start").hide();
-      $("#choices").hide();
-      showDisplay();
-      firstQuestion();
-      timer();
+    questionNumber = 0;
+    correctGuess = 0;
+    wrongGuess = 0;
+    time = 16;
+    startGame();
+  });
+  $(".answer-choices").on("click", function() {
+    if (checkAnswer($(this).html()) === true) {
+      correctGuess++;
+      questionNumber++;
+      askQuestion(questionNumber);
+    } else if (checkAnswer($(this).html()) === false) {
+      wrongGuess++;
+      questionNumber++;
+      askQuestion(questionNumber);
     }
   });
-  //On next button click, show question 2//
-  $("#next").unbind("click");
-  $("#next").on("click", function() {
-    showDisplay();
-    secondQuestion();
-    $("#next").hide();
-    $("#rightOrWrong").hide();
-    $("#explain").hide();
-    time = 30;
-    timer();
+  $("#replay").on("click", function() {
+    startGame();
+    $("#wins").fadeOut();
+    $("#losses").fadeOut();
   });
-  //On next button click, show question 3//
-  $("#next2").unbind("click");
-  $("#next2").on("click", function() {
-    showDisplay();
-    thirdQuestion();
-    $("#next2").hide();
-    $("#rightOrWrong").hide();
-    $("#explain").hide();
-    time = 30;
-    timer();
-  });
-  //On next button click, show question 4//
-  $("#next3").unbind("click");
-  $("#next3").on("click", function() {
-    showDisplay();
-    fourthQuestion();
-    $("#next3").hide();
-    $("#rightOrWrong").hide();
-    $("#explain").hide();
-    time = 30;
-    timer();
-  });
-  //On next button click, show question 5//
-  $("#next4").unbind("click");
-  $("#next4").on("click", function() {
-    showDisplay();
-    fifthQuestion();
-    $("#next4").hide();
-    $("#rightOrWrong").hide();
-    $("#explain").hide();
-    time = 30;
-    timer();
-  });
-  //On next button click, show question 6//
-  $("#next5").unbind("click");
-  $("#next5").on("click", function() {
-    showDisplay();
-    sixthQuestion();
-    $("#next5").hide();
-    $("#rightOrWrong").hide();
-    $("#explain").hide();
-    time = 30;
-    timer();
-  });
-  //Display result on result button click//
 });
